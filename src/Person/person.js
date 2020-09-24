9de3b0c4b0693d393f07723e2861ea7e7b01c6be
Siddharth Vara <siddharth.vara@gamil.com>
2020-09-24 12:03:01 +0530

    applying css to component.

--- src/Person/person.js
+++ src/Person/person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './person.css';
 
 const Person = (props) => {
     return (
-    <div>
+    <div className="person">
         <h1>custom component created by { props.name } and Age: { props.age }</h1>
         <input type='text' onChange={props.change} value={ props.name }/>
     </div>
