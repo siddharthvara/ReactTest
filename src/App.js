96a61ccd86b4c3e2524cd872fe81c5521f1c030d
Siddharth Vara <siddharth.vara@gamil.com>
2020-09-24 12:32:50 +0530

    Implement Inline css.

--- src/App.js
+++ src/App.js
@@ -36,10 +36,23 @@ function App() {
     return <Person name= {res.name} age = {res.age} change = {toggleNameHandler}></Person>;
   })
 
+  const style = {
+    padding: "15px 25px",
+    fontSize: "24px",
+    textAlign: "center",
+    cursor: "pointer",
+    outline: "none",
+    color: "#fff",
+    backgroundColor: "#4CAF50",
+    border: "none",
+    borderRadius: "15px",
+    boxShadow: "0 9px #999"
+  };
+
   return (
     <div className="App">
       <h1>My First React App</h1>
-      <button onClick = {changeNameHandler} >Switch Name</button>
+      <button style= {style} onClick = {changeNameHandler} >Switch Name</button>
       { per }
     </div>
   );
